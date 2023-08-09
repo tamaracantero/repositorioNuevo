23f6d8347136e129228575a983603e99a2df205a
Joaquin Frasca <joaqokinkinhd@gmail.com>
2023-08-09 11:40:34 -0300

    Update GitHub.java

--- gitHub/src/github/GitHub.java
+++ gitHub/src/github/GitHub.java
@@ -15,7 +15,8 @@ public class GitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Pinche git"); // hecho por Joaquin
     }
     
 }
