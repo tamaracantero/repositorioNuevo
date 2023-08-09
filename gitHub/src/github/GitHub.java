6b4f600392de8f719736726363f63d8a2f28b6da
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2023-08-09 12:06:24 -0300

    limpiar pantalla
    robot limpia pantalla, preciona los botones ctr + L automaticamente

--- gitHub/src/github/GitHub.java
+++ gitHub/src/github/GitHub.java
@@ -5,6 +5,9 @@
  */
 package github;
 
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 /**
  *
  * @author valkiria
@@ -14,12 +17,31 @@ public class GitHub {
     /**
      * @param args the command line arguments
      */
+    
+        public static void limpiarPant(){
+        try{
+            System.out.println("Presione enter para continuar...");
+            new java.util.Scanner(System.in).nextLine();
+
+            Robot limpiar = new Robot();
+            limpiar.keyPress(KeyEvent.VK_CONTROL);
+            limpiar.keyPress(KeyEvent.VK_L);
+            limpiar.keyRelease(KeyEvent.VK_CONTROL);
+            limpiar.keyRelease(KeyEvent.VK_L);
+            
+        }  catch (Exception e) {
+            System.out.println("Error al limpiar la pantalla"+e.getMessage());
+        }
+    }
+    
     public static void main(String[] args) {
         
         System.out.println("Pinche git"); // hecho por Joaquin
         System.out.println("Pinche net beans"); //hecho Juan
         System.out.println("hola compas");//hecho por tamara
         System.out.println("Pipipipi");//by Tomi Rivero
+        
+        limpiarPant();
     }
     
 }
