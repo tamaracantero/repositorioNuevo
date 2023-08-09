55f70a72651c48d639292dd62de9272396f9198f
JuanLaspiur <laspiurperezjuan@gmail.com>
2023-08-09 12:08:04 -0300

    commit area y perimetro cuadrilatero

--- gitHub/src/github/GitHub.java
+++ gitHub/src/github/GitHub.java
@@ -20,6 +20,21 @@ public class GitHub {
         System.out.println("Pinche net beans"); //hecho Juan
         System.out.println("hola compas");//hecho por tamara
         System.out.println("Pipipipi");//by Tomi Rivero
+
+        /*) En un nuevo proyecto crear una clase de nombre Cuadrilatero con los atributos largo y alto, un
+constructor que permita inicializar dichos atributos, sus respectivos getter y setter y los siguientes
+métodos adicionales:
+ calcularPerimetro(): este método retornará el perímetro del cuadrilátero.
+ calcularArea(): este método retornará la superficie del cuadrilátero.
+ esUnCuadrado(): este método retornará true si este cuadrilátero es un cuadrado, caso
+contrario retornará false.
+Luego desde la clase principal del proyecto (la que contiene el método main) se pide:
+a) Crear un objeto Cuadrilatero con largo 70 y altura 50;
+Luego utilizando sus métodos:
+a) Mostrar por consola su perímetro
+b) Mostrar por consola su superficie.
+c) Mostrar por consola si es un cuadrado o no.
+d) Cambiar el estado de uno de sus atributos para que sea un cuadrado*/
     }
     
 }
