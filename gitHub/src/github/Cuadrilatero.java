f55f217c3e48ad20e06e5c0541967d6170451be5
tamaracantero <141643686+tamaracantero@users.noreply.github.com>
2023-08-09 12:42:08 -0300

    constructor y getters and setters

--- gitHub/src/github/Cuadrilatero.java
+++ gitHub/src/github/Cuadrilatero.java
@@ -5,4 +5,27 @@ public class Cuadrilatero {
     //atributos
     private double alto;
     private double largo;
+    //Constructor
+
+    public Cuadrilatero(double alto, double largo) {
+        this.alto = alto;
+        this.largo = largo;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+    
 }
